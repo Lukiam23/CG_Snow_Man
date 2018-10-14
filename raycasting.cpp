cc750d187dabe6531f058698f069ba9c6c2f176f
Lukiam23 <mscorreia95@gmail.com>
2018-10-14 23:01:23 +0000

    Mudança do snowman

--- raycasting.cpp
+++ raycasting.cpp
@@ -1,7 +1,7 @@
 #include <cmath>
 #include <iostream>
 #include <limits>
-#include "scenario.h"
+#include "scenario2.h"
 
 #ifdef __APPLE__
 #include <GL/glew.h>
@@ -26,162 +26,217 @@ static float          W = 50., H = 50.;
 
 // Initialization routine.
 void setup(void) {
-    glClearColor(1.0, 1.0, 1.0, 0.0);
+	glClearColor(1.0, 1.0, 1.0, 0.0);
 };
 
 float x, y;
 
-float Dx = W / windowW;
-float Dy = H / windowH;
+float Dx;
+float Dy;
 
-Point O(250, 250, -500); // Origin's point of camera
+Point O(0,10,1000); // Origin's point of camera
 
-Point light_center(250, 300, -400);
-Vector light_color(0.5, 0.5, 0.5);
+Point light_center(0, -10000,0);
+Vector light_color(1, 1, 1);
 Light light(light_center, light_color);
 
 // Setting snow material
 Vector snow_env_material(1, 1, 1);       // Material's enviroment component factors
 Vector snow_dif_material(0.1, 0.1, 0.1);       // Material's difuse component factors
-Vector snow_spe_material(1, 1, 1); // Material's specular component factors
+Vector snow_spe_material(0.4, 0.5, 0.5); // Material's specular component factors
 Material snow_material(snow_env_material, snow_dif_material, snow_spe_material);
 
-Vector snow_env_material2(1, 1, 1);       
-Vector snow_dif_material2(0.1, 0.1, 0.1);       
-Vector snow_spe_material2(1, 1, 1); 
-Material snow_material2(snow_env_material2, snow_dif_material2, snow_spe_material2);
 
-Vector snow_env_materialEye(0, 0, 0);       
-Vector snow_dif_materialEye(0.6, 0.6, 0.6);      
-Vector snow_spe_materialEye(1, 1, 1); 
+Vector snow_env_materialEye(0, 0, 0);       // Material's enviroment component factors
+Vector snow_dif_materialEye(0.6, 0.6, 0.6);       // Material's difuse component factors
+Vector snow_spe_materialEye(1, 1, 1); // Material's specular component factors
 Material snow_materialEye(snow_env_materialEye, snow_dif_materialEye, snow_spe_materialEye);
+
+Vector snow_env_materialBottom1(0, 0, 0);       // Material's enviroment component factors
+Vector snow_dif_materialBottom1(0.6, 0.6, 0.6);       // Material's difuse component factors
+Vector snow_spe_materialBottom1(1, 1, 1); // Material's specular component factors
+Material snow_materialBottom1(snow_env_materialBottom1, snow_dif_materialBottom1, snow_spe_materialBottom1);
+
+Vector snow_env_materialBottom2(0, 0, 0);       // Material's enviroment component factors
+Vector snow_dif_materialBottom2(0.6, 0.6, 0.6);       // Material's difuse component factors
+Vector snow_spe_materialBottom2(1, 1, 1); // Material's specular component factors
+Material snow_materialBottom2(snow_env_materialBottom2, snow_dif_materialBottom2, snow_spe_materialBottom2);
+
+Vector snow_env_materialBottom3(0, 0, 0);       // Material's enviroment component factors
+Vector snow_dif_materialBottom3(0.6, 0.6, 0.6);       // Material's difuse component factors
+Vector snow_spe_materialBottom3(1, 1, 1); // Material's specular component factors
+Material snow_materialBottom3(snow_env_materialBottom3, snow_dif_materialBottom3, snow_spe_materialBottom3);
+
+
 
 
 // Snowman definition
 //Point eye_center();
-Point eye_center1(230, 365, 100); // Center of body sphere
-Sphere eye1(eye_center1, 10, snow_materialEye);
+Point eye_center1(5, 22, -12); // Center of eye sphere
+Sphere eye1(eye_center1, 2, snow_materialEye);
 
-Point eye_center2(260, 365, 100); // Center of body sphere
-Sphere eye2(eye_center2, 10, snow_materialEye);
+Point eye_center2(-5, 22, -12); // Center of eye sphere
+Sphere eye2(eye_center2, 2, snow_materialEye);
 
-Point body_center(250, 200, 1); // Center of body sphere
-Sphere body(body_center, 100, snow_material2);
+Point nose_center(0, 20, -12); // Center of eye sphere
+Sphere nose(nose_center, 2, snow_materialEye);
 
-Point head_center(250,330, 1); // Center of head sphere
-Sphere head(head_center, 40, snow_material);
+Point bottom_center1(0, -20, -17); // Center of bottom sphere
+Sphere bottom1(bottom_center1, 2, snow_materialBottom1);
+
+Point bottom_center2(0, -10, -20); // Center of bottom sphere
+Sphere bottom2(bottom_center2, 2, snow_materialBottom2);
+
+Point bottom_center3(0, 0, -20); // Center of bottom sphere
+Sphere bottom3(bottom_center3, 2, snow_materialBottom3);
+
+Point body_center(0, -10, 0); // Center of body sphere
+Sphere body(body_center, 20, snow_material);
+
+Point head_center(0, 20, 0); // Center of head sphere
+Sphere head(head_center, 10, snow_material);
 
 
-int n = 4; // Number of objects (spheres) in scenario
-Sphere objects[] = {body, head,eye1,eye2}; // List of these objects
+
+int n = 8; // Number of objects (spheres) in scenario
+Sphere objects[] = { body,head,eye1,eye2,bottom1,bottom2,bottom3 }; // List of these objects
+
+
+Point lookat(0, 20, 0);
+Point viewUp(0, 30, 0);
+
+Vector lookatO(O.x - lookat.x, O.y - lookat.y, O.z - lookat.z); //Vecto lookat->O is created
+Vector kc = lookatO.normalize();//Vector lookat->O normalized (0,-1,0)
+Vector Vup(viewUp.x - O.x, viewUp.y - O.y, viewUp.z - O.z); //(0,30,0)
+
+Vector I = Vup.vectorialProduct(kc); //(0,0,0)
+Vector ic = I.normalize();
+Vector jc = kc.vectorialProduct(ic);
+float Mw_c[4][4] = { {ic.x,ic.y,ic.z, -(ic.dot(O))}, {jc.x, jc.y, jc.z, -(jc.dot(O))}, {kc.x , kc.y, kc.z, -(kc.dot(O))}, {0,0,0,1} };
+
+
+
+
+
+
+
+
+
+void setObjetos() {
+	for (int i = 0; i < n; i++) {
+		objects[i].center.changeCamera(Mw_c);
+	}
+}
 
 // Drawing routine.
 void drawScene(void) {
-    glClear(GL_COLOR_BUFFER_BIT);
+	glClear(GL_COLOR_BUFFER_BIT);
+	Dx = W / windowW;
+	Dy = H / windowH;
 
-    glColor3f(0.0, 0.0, 0.0);
-
-    glEnable(GL_PROGRAM_POINT_SIZE_EXT);
-    glPointSize(1);
+	glEnable(GL_PROGRAM_POINT_SIZE_EXT);
+	glPointSize(1);
 	glLoadIdentity();
 
 	glTranslated(0, 0, -2.5);
-    glBegin(GL_POINTS);
-        for (float i = 0; i < windowH; i += 1) {
-			x = H / 2 - Dy / 2 - i * Dy;
-            for (float j = 0; j < windowW; j += 1) {
-				y = -W / 2 + Dx / 2 + j * Dx;
-                float d = -10; // Position of window
-                Point Pix(i, j, d); // Pixel point in window
-				float t_int = numeric_limits<float>::max();
-				float t = 0;
-				Vector V(O, Pix); // Vector from camera to Pixel
-				// Setting the enviroment light;
-				Vector Ienv(0.3921, 0.5843, 0.9294);
-				float deltaRay = -1;
-				int indexSphere = 0;
+	glBegin(GL_POINTS);
+	for (float i = 0; i < windowH; i++) {
+		y = H / 2 - Dy / 2 - i * Dy;
+		for (float j = 0; j < windowW; j++) {
+			x = -W / 2 + Dx / 2 + j * Dx;
+			float d = 200; // Position of window
+			Point Pix(x, y, d); // Pixel point in window
 
-				for (int k = 0; k < n; k++) {
-					// Checking if the ball is intercepted by ray
-					// This check uses the square equation delta
-					float alpha = V.dot(V);
+			float t_int = numeric_limits<float>::max();
+			float t = numeric_limits<float>::max();
 
-					Vector CO(objects[k].center.x - O.x, objects[k].center.y - O.y, objects[k].center.z - O.z);
-					float beta = 2 * V.dot(CO);
+			Vector V(O.x - Pix.x, O.y - Pix.y, O.z - Pix.z); // Vector from camera to Pixel
+			// Setting the enviroment light;
+			Vector Ienv(0.3921, 0.5843, 0.9294);
+			float deltaRay = -1;
+			int indexSphere = -1;
 
-					float gamma = CO.dot(CO) -pow(objects[k].radius, 2);
+			for (int k = 0; k < n; k++) {
+				// Checking if the ball is intercepted by ray
+				// This check uses the square equation delta
+				float alpha = V.dot(V);
 
-					// Calculating the delta from 'alpha', 'beta' and 'gamma';
-					float delta = beta * beta - 4 * alpha * gamma;
-					
-					if (delta >= 0) {
-						t = min((-beta + sqrt(delta)) / (2 * alpha), (-beta - sqrt(delta)) / (2 * alpha));
-						if (t < t_int) {
-							t_int = t;
-							indexSphere = k;
-							deltaRay = delta;
-						}
+				Vector CO(O.x - objects[k].center.x, O.y - objects[k].center.y, O.z - objects[k].center.z);
+				float beta = 2 * V.dot(CO);
+
+				float gamma = CO.dot(CO) - pow(objects[k].radius, 2);
+
+				// Calculating the delta from 'alpha', 'beta' and 'gamma';
+				float delta = beta * beta - 4 * alpha * gamma;
+
+				if (delta >= 0) {
+					t = min((-beta + sqrt(delta)) / (2 * alpha), (-beta - sqrt(delta)) / (2 * alpha));
+					if (t < t_int) {
+						t_int = t;
+						indexSphere = k;
+						deltaRay = delta;
 					}
 				}
-                    
-                // Vector Ienv(0, 1, 0);
-                if (deltaRay >= 0) {
-                    // Scalar that stretch the 'V' vector from camera to intercepted point
-                        
-						
-					V.times(t_int); // V now is a Vector from the camera to the point in the object
+			}
 
-					// Find the intercepted point
-					Point P(O);
-					P.plus(V);//Point whose the vector V intercept
+			// Vector Ienv(0, 1, 0);
+			if (deltaRay >= 0) {
+				// Scalar that stretch the 'V' vector from camera to intercepted point
 
-					Vector N(objects[indexSphere].center, P);
-					Vector n = N.normalize(); // Normal vector to point in sphere surface
 
-					Vector L(P, light.center);
-					Vector l = L.normalize(); // Nomalized vector from point to light
+				V.times(t_int); // V now is a Vector from the camera to the point in the object
 
-					Vector Kenv(objects[indexSphere].material.env_material); // Components factors to enviroment light
-					Vector Kdif(objects[indexSphere].material.dif_material); // Components factors to difuse light
-					Vector Kspe(objects[indexSphere].material.spe_material);  // Components factors to specular light
+				// Find the intercepted point
+				Point P(O);
+				P.plus(V);//Point whose the vector V intercept
 
-					Vector If(light.color); // Light rate
+				Vector N(objects[indexSphere].center, P);
+				Vector n = N.normalize(); // Normal vector to point in sphere surface
 
-					// Calculating the difusing rate
-					Vector Idif(If);
-					Idif.at(Kdif);
-					float Fdif = l.dot(n);
-					Idif.times(Fdif);
+				Vector L(P, light.center);
+				Vector l = L.normalize(); // Nomalized vector from point to light
 
-					// Calculating the specular rate
-					Vector Ispe(If);
-					Ispe.at(Kspe);
-					Vector r(n);
-					r.times(2 * l.dot(n));
-					r.less(l);
-					Vector PO(P, O);
-					Vector v = PO.normalize();
-					float Fspe = pow(r.dot(v),3);
-					Ispe.times(Fspe);
+				Vector Kenv(objects[indexSphere].material.env_material); // Components factors to enviroment light
+				Vector Kdif(objects[indexSphere].material.dif_material); // Components factors to difuse light
+				Vector Kspe(objects[indexSphere].material.spe_material);  // Components factors to specular light
 
-					// Generating the final color for current pixel
-					Vector Color(Ienv);
-					Color.at(Kenv);
-					Color.plus(Idif);
-					Color.plus(Ispe);
+				Vector If(light.color); // Light rate
 
-					glColor3f(Color.x, Color.y, Color.z);
-                }
-				else {
-					glColor3f(Ienv.x, Ienv.y, Ienv.z);
-				}
-      
-                glVertex3f(x,y,-1);
-            }
-        }
-    glEnd();
+				// Calculating the difusing rate
+				Vector Idif(If);
+				Idif.at(Kdif);
+				float Fdif = l.dot(n);
+				Idif.times(Fdif);
 
-    glFlush();
+				// Calculating the specular rate
+				Vector Ispe(If);
+				Ispe.at(Kspe);
+				Vector r(n);
+				r.times(2 * l.dot(n));
+				r.less(l);
+				Vector PO(P, O);
+				Vector v = PO.normalize();
+				float Fspe = pow(r.dot(v), 1);
+				Ispe.times(Fspe);
+
+				// Generating the final color for current pixel
+				Vector Color(Ienv);
+				Color.at(Kenv);
+				Color.plus(Idif);
+				Color.plus(Ispe);
+
+				glColor3f(Color.x, Color.y, Color.z);
+			}
+			else {
+				glColor3f(Ienv.x, Ienv.y, Ienv.z);
+			}
+
+			glVertex2f(x, y);
+		}
+	}
+	glEnd();
+
+	glFlush();
 }
 
 // OpenGL window reshape routine.
@@ -211,31 +266,32 @@ void resize(int w, int h)
 
 // Keyboard input processing routine.
 void keyInput(unsigned char key, int x, int y) {
-    switch (key) {
-        case 27:
-            exit(0);
-            break;
-        default:
-            break;
-    }
+	switch (key) {
+	case 27:
+		exit(0);
+		break;
+	default:
+		break;
+	}
 }
 
 // Main routine.
-int main(int argc, char **argv){
-    glutInit(&argc, argv);
+int main(int argc, char **argv) {
+	glutInit(&argc, argv);
+	//setObjetos();
+	setObjetos();
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
+	glutInitWindowSize(windowW, windowH);
+	glutInitWindowPosition(000, 000);
+	glutCreateWindow("Ray Casting");
+	glutDisplayFunc(drawScene);
+	glutReshapeFunc(resize);
+	glutKeyboardFunc(keyInput);
 
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
-    glutInitWindowSize(windowW, windowH);
-    glutInitWindowPosition(000, 000);
-    glutCreateWindow("Ray Casting");
-    glutDisplayFunc(drawScene);
-    glutReshapeFunc(resize);
-    glutKeyboardFunc(keyInput);
+	glewExperimental = GL_TRUE;
+	glewInit();
 
-    glewExperimental = GL_TRUE;
-    glewInit();
+	setup();
 
-    setup();
-
-    glutMainLoop();
+	glutMainLoop();
 }
--- scenario.h
+++ scenario.h
@@ -1,12 +1,15 @@
 #include <cmath>
 #include <iostream>
 
+using namespace std;
+
 class Vector;
 
 float min(float x, float y) {
     if (x > y) {
         return y;
-    }else{
+    }
+    else {
         return x;
     }
 };
@@ -19,15 +22,29 @@ public:
     Point(const Point&);
     Point(float, float, float);
     void plus(const Vector&);
+    void changeCamera(float[4][4]);
 };
 
-Point::Point (const Point& p) {
+void Point::changeCamera(float Mw_c[4][4]) {
+    float v[4] = { x,y,z,1 };
+    float newPosition[4] = { 0,0,0,0 };
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            newPosition[i] += v[j] * Mw_c[i][j];
+        }
+    }
+    x = newPosition[0];
+    y = newPosition[1];
+    z = newPosition[2];
+};
+
+Point::Point(const Point& p) {
     x = p.x;
     y = p.y;
     z = p.z;
 };
 
-Point::Point (float cx, float cy, float cz) {
+Point::Point(float cx, float cy, float cz) {
     x = cx;
     y = cy;
     z = cz;
@@ -49,71 +66,82 @@ public:
     void times(float);
     float norm();
     Vector normalize();
+    Vector vectorialProduct(Vector);
 };
 
-float Vector::norm ()  {
+//Vectorial product for matrix of length 3
+Vector Vector::vectorialProduct(Vector v2) {
+    Vector product(y * v2.z - z * v2.y, z * v2.x - x * v2.z, x * v2.y - y * v2.x);
+    return product;
+};
+
+float Vector::norm() {
     return sqrt(x * x + y * y + z * z);
 };
 
-Vector Vector::normalize ()  {
+Vector Vector::normalize() {
     float norm = this->norm();
-    Vector normalized(x * 1 / norm, y * 1 / norm, z * 1 / norm);
-    return normalized;
+    if (norm != 0) {
+        Vector normalized(x * 1 / norm, y * 1 / norm, z * 1 / norm);
+        return normalized;
+    }
+    return Vector(x, y, z);
+    
 };
 
-float Vector::dot (Vector v) {
+float Vector::dot(Vector v) {
     return x * v.x + y * v.y + z * v.z;
 };
 
-void Vector::at (Vector v) {
+void Vector::at(Vector v) {
     x *= v.x;
     y *= v.y;
     z *= v.z;
 };
 
-void Vector::plus (Vector v) {
+void Vector::plus(Vector v) {
     x += v.x;
     y += v.y;
     z += v.z;
 };
 
-void Vector::less (Vector v) {
+void Vector::less(Vector v) {
     x -= v.x;
     y -= v.y;
     z -= v.z;
 };
 
-void Vector::times (float t) {
+void Vector::times(float t) {
     x *= t;
     y *= t;
     z *= t;
 };
 
-Vector::Vector (float cx, float cy, float cz) {
+Vector::Vector(float cx, float cy, float cz) {
     x = cx;
     y = cy;
     z = cz;
 };
 
-Vector::Vector (Point p1, Point p2) {
+Vector::Vector(Point p1, Point p2) {
     x = p2.x - p1.x;
     y = p2.y - p1.y;
     z = p2.z - p1.z;
 };
 
-Vector::Vector (Point p) {
+Vector::Vector(Point p) {
     x = p.x;
     y = p.y;
     z = p.z;
 };
 
-Vector::Vector (const Vector& v) {
+Vector::Vector(const Vector& v) {
     x = v.x;
     y = v.y;
     z = v.z;
 };
 
-void Point::plus (const Vector& v) {
+void Point::plus(const Vector& v) {
     x += v.x;
     y += v.y;
     z += v.z;
@@ -140,8 +168,8 @@ public:
     Sphere(Point, float, const Material&);
 };
 
-Sphere::Sphere(Point pcenter, float sradius, const Material& smaterial) 
-: center(pcenter), material(smaterial.env_material, smaterial.dif_material, smaterial.spe_material) {
+Sphere::Sphere(Point pcenter, float sradius, const Material& smaterial)
+    : center(pcenter), material(smaterial.env_material, smaterial.dif_material, smaterial.spe_material) {
     radius = sradius;
 };
 
@@ -153,3 +181,21 @@ public:
 };
 
 Light::Light(Point lcenter, Vector lcolor) : center(lcenter), color(lcolor) {}
+
+class Camera {
+public:
+    Point lookat;
+    Point viewUp;
+    Point Origem;
+    Camera(Point, Point, Point);
+    void setCamera();
+    void setMatrix();
+};
+
+Camera::Camera(Point l, Point v, Point o) : lookat(l), viewUp(v), Origem(o) {}
+
+void Camera::setCamera() {
+
+};
+
+
